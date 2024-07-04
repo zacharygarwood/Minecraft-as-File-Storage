5fc3344a8527ff8ee9ff68b93aa6bb7f1d66ead8
Zachary Garwood <garwoodzachary@gmail.com>
2024-07-04 16:37:23 -0500

    Setup Spigot plugin

---
+++ src/main/java/com/mafs/Plugin.java
@@ -0,0 +1,14 @@
+package com.mafs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("onEnable is called!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("onDisable is called!");
+    }
+}
