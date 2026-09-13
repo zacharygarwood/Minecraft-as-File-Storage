c967bcae3b57eeabb60d89a35c6486a54b9c2c74
Zach Garwood <garwoodzachary@gmail.com>
2026-09-13 17:14:23 -0500

    feat: Add strict block to byte lookup

--- src/main/java/com/mafs/helper/Mapping.java
+++ src/main/java/com/mafs/helper/Mapping.java
@@ -2,20 +2,29 @@ package com.mafs.helper;
 
 import org.bukkit.Material;
 
+import java.util.EnumMap;
+import java.util.Map;
+
 import static com.mafs.helper.Constants.MATERIALS;
 
 public class Mapping {
+    private static final Map<Material, Byte> BYTES = new EnumMap<>(Material.class);
+
+    static {
+        for (int i = 0; i < MATERIALS.length; i++) {
+            BYTES.put(MATERIALS[i], (byte) i);
+        }
+    }
+
     public static Material byteToBlock(byte b) {
-        int index = b & 0xff;
-        return MATERIALS[index];
+        return MATERIALS[b & 0xff];
     }
 
     public static byte blockToByte(Material material) {
-        for (int i = 0; i < MATERIALS.length; i++) {
-            if (MATERIALS[i].equals(material)) {
-                return (byte) i;
-            }
+        Byte b = BYTES.get(material);
+        if (b == null) {
+            throw new IllegalArgumentException("No byte is mapped to " + material);
         }
-        return -1;
+        return b;
     }
 }
